e31ac484b702d9df6e1996a448952a683c25219f
Kliiim89 <mark1989mail@mail.ru>
2022-08-02 20:15:57 +0300

    Задачи

--- Up62/Program.cs
+++ Up62/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 62. Заполните спирально массив 4 на 4.
+
+Console.Clear();
+int n = 4;
+int[,] sqareMatrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+{
+  sqareMatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < sqareMatrix.GetLength(01) - 01)
+    j++;
+  else if (i < j && i + j >= sqareMatrix.GetLength(00) - 01)
+    i++;
+  else if (i >= j && i + j > sqareMatrix.GetLength(01) - 01)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(sqareMatrix);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(00); i++)
+  {
+    for (int j = 0; j < array.GetLength(01); j++)
+    {
+      if (array[i,j] / 10 <= 00)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
